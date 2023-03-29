bc2195e692ab04a98d8ce094333c30a7c54e3981
danielsilva <daniel.silva@simberg.com.br>
2023-03-29 16:01:55 -0300

    fix: fixed issues

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,13 @@
 $(document).ready(function () {
-    const inputElement = $('#input');
-    const outputElement = $('#output');
-    const finalElement = $('#final');
-    const btnDoneElement = $('#done');
-    const btnRetryElement = $('#retry');
-    
-    inputElement.filepond();
-
-    $.fn.filepond.setDefaults({
-      acceptedFileTypes: ['image/*'],
-      onpreparefile: onPrepareFile,
-      onremovefile: onRemoveFile,
-
-    });
+    const inputElement = $("#input");
+    const outputElement = $("#output");
+    const finalElement = $("#final");
+    const btnDoneElement = $("#done");
+    const btnRetryElement = $("#retry");
 
     function onPrepareFile(file, output) {
       outputElement
-        .attr('src', URL.createObjectURL(output))
+        .attr("src", URL.createObjectURL(output))
         .show()
         .cropper();
       btnDoneElement.show();
@@ -26,15 +17,45 @@ $(document).ready(function () {
       btnDoneElement.hide();
       btnRetryElement.hide();
       finalElement.hide();
-      outputElement.hide().cropper('destroy');
+      outputElement.hide().cropper("destroy");
     }
 
-    btnRetryElement.click(() => inputElement.filepond('removeFile'));
-    
+    $.fn.filepond.registerPlugin(
+      // FilePondPluginImagePreview,
+      FilePondPluginFileValidateSize,
+      FilePondPluginFileValidateType,
+      // FilePondPluginImageExifOrientation,
+      FilePondPluginImageTransform
+      // FilePondPluginImageCrop,
+      // FilePondPluginImageResize,
+      // FilePondPluginFileMetadata,
+    );
+
+    $.fn.filepond.setDefaults({
+      maxFileSize: "5MB",
+      acceptedFileTypes: ["image/*"],
+      onpreparefile: onPrepareFile,
+      onremovefile: onRemoveFile,
+      // imagePreviewHeight: "423", // 2480 x 3508
+      // imageResizeTargetWidth: 2480,
+      // imageResizeTargetHeight: 3508,
+
+      // imageCropAspectRatio: '1:1',
+      // imageResizeTargetWidth: 384,
+
+      // imageResizeMode: 'cover',
+      // imageResizeTargetHeight: 2480,
+      // imageResizeTargetWidth: 3508,
+      // imageResizeUpscale: false,
+    });
+
+    inputElement.filepond();
+
+    btnRetryElement.click(() => inputElement.filepond("removeFile"));
     btnDoneElement.click(() => {
-      outputElement.cropper('getCroppedCanvas').toBlob((blob) => {
-        outputElement.hide().cropper('destroy');
-        finalElement.attr('src', URL.createObjectURL(blob)).show();
+      outputElement.cropper("getCroppedCanvas").toBlob((blob) => {
+        outputElement.hide().cropper("destroy");
+        finalElement.attr("src", URL.createObjectURL(blob)).show();
       })
     });
   });
